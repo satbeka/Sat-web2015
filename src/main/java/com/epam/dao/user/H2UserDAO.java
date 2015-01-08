9a0921759f9b1e63f0c47b183dabbc28d87a29bb
satbeka <satbeka@gmail.com>
2015-01-08 18:45:31 +0600

    020115

---
+++ src/main/java/com/epam/dao/user/H2UserDAO.java
@@ -0,0 +1,119 @@
+package com.epam.dao.user;
+
+
+import com.epam.db.ConnectionPool;
+import com.epam.model.Administrator;
+import com.epam.model.Client;
+import com.epam.model.User;
+
+import javax.sql.RowSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class H2UserDAO implements UserDAO{
+
+
+    private Connection connection = null;
+
+    // initialization
+    public H2UserDAO() {
+    }
+
+    public Connection setConnection(ConnectionPool connectionPool) {
+        if (this.connection==null){
+            this.connection=connectionPool.takeConnection();};
+        return this.connection;
+    }
+
+    public void closeConnection(ConnectionPool connectionPool) {
+        if (this.connection!=null){
+            connectionPool.releaseConnection(this.connection);};
+        return;
+    }
+
+    @Override
+    public long insertUser(User user) {
+        return 0;
+    }
+
+    @Override
+    public boolean deleteUser(User user) {
+        return false;
+    }
+
+    @Override
+    public User findUserByName(String name) {
+        return null;
+    }
+
+    @Override
+    public User findUserByLogin(String login) {
+        //User user=new User();
+        Connection cn=this.connection;
+
+        PreparedStatement st = null;
+        try {
+            st = cn.prepareStatement("select * from user where login =? and nvl(deleted,0)!=1;");
+            st.setString(1,login);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ResultSet rs = null;
+        Long role=null;
+        try {
+            rs=st.executeQuery();
+            rs.next();
+            role=rs.getLong(3);
+            if (role==1) {
+                Administrator administrator=null;
+                administrator.setId(rs.getLong(1));
+                administrator.setName(rs.getString(2));
+                administrator.setBirthDay(rs.getDate(8));
+                administrator.setInn(rs.getString(7));
+                administrator.setPassword(rs.getString(6));
+                return administrator;}
+
+            Client client=null;
+            client.setId(rs.getLong(1));
+            client.setName(rs.getString(2));
+            client.setBirthDay(rs.getDate(8));
+            client.setInn(rs.getString(7));
+            client.setPassword(rs.getString(6));
+            return client;
+
+        } catch (SQLException e) {
+            //TODO log e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return null;
+    }
+
+    @Override
+    public User findFirstUserByName(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    @Override
+    public RowSet selectUserRS(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> selectUserTO() {
+        return null;
+    }
+}
--- src/main/java/com/epam/validation/Validator.java
+++ src/main/java/com/epam/validation/Validator.java
@@ -4,6 +4,8 @@ package com.epam.validation;
 //import java.sql.Date;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Validator {
 
@@ -17,6 +19,17 @@ public class Validator {
     public boolean isNumber(Number number) {
         return true;
     };
+    public boolean isLoginCorrect(String login){
+        String regex="[a-zA-Z_0-9]*";
+        Pattern p=Pattern.compile(regex);
+        Matcher matcher = p.matcher(login);
+        if (matcher.matches()){
+            return true;
+        }
+        else
+            return false;
+
+    }
 
 
 }
